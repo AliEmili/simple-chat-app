98418e2cc7b4858cf429c80294ba762718be08eb
aliemili <aliemili2013@yahoo.com>
2021-12-31 15:19:52 +0330

    providing basic store

---
+++ src/Store/configureStore.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./Reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(thunk));
+  const persistore = persistStore(store);
+  return {
+    store,
+    persistStore,
+  };
+};
